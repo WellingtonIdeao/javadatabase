ac2d58fc1a702d486b0c96ada1ffcb9f99dba44c
Wellington Ideão <wellington.ideao@gmail.com>
2026-07-24 20:14:28 -0300

    Processar instruções sql com o JDBC
    Faz um processo completo de uma instrução sql no banco de dados.
    O processo consiste em:
     - abrir a conexão com o banco de dados;
     - executar a instrução  sql;
     - processar os dados obtidos;
     - e fechar a conexão anteriormente aberta.

--- src/main/java/com/ideao/dev/javadatabase/App.java
+++ src/main/java/com/ideao/dev/javadatabase/App.java
@@ -3,12 +3,25 @@ package com.ideao.dev.javadatabase;
 import com.ideao.dev.javadatabase.util.JdbcConnection;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class App {
 	public static void main(String[] args) {
-		try (Connection connection = JdbcConnection.getConnection()) {
+		try (Connection con = JdbcConnection.getConnection();
+			 Statement stmt = con.createStatement()) {
 			System.out.println("Hello World, Java database project");
+			stmt.executeUpdate("drop table if exists person");
+			stmt.executeUpdate("create table person (id integer, name string)");
+			stmt.executeUpdate("insert into person values (1, 'Guga')");
+			stmt.executeUpdate("insert into person values (2, 'José')");
+			ResultSet rs = stmt.executeQuery("select * from person");
+
+			while (rs.next()) {
+				System.out.print("id = " + rs.getInt("id"));
+				System.out.println(", name = " + rs.getString("name"));
+			}
 		} catch (SQLException e) {
             throw new RuntimeException(e);
         }
