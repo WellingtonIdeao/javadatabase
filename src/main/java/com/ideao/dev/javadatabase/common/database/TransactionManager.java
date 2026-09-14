449e57e3556e16252e74efe269ea8c5eecb1e8e2
Wellington Ideão <wellington.ideao@gmail.com>
2026-09-14 16:58:37 -0300

    fix: remover código boilerplate da transação

--- src/main/java/com/ideao/dev/javadatabase/common/database/TransactionManager.java
+++ src/main/java/com/ideao/dev/javadatabase/common/database/TransactionManager.java
@@ -11,38 +11,10 @@ import java.util.function.Function;
 public class TransactionManager {
 
     public static void execute(Consumer<EntityManager> action) {
-        EntityManager em = null;
-        EntityTransaction tx = null;
-        Throwable mainError = null;
-        try {
-            try {
-                em = JPASingleton.getInstance().getEntityManager();
-                tx = em.getTransaction();
-                tx.begin();
-                action.accept(em);
-                tx.commit();
-            } catch (Throwable t) {
-                if (tx != null && tx.isActive()) {
-                    tx.rollback();
-                }
-                mainError = t;
-                throw t;
-            } finally {
-                    if (mainError != null) {
-                        try {
-                           JPASingleton.getInstance().closeEntityManager();
-                        } catch (Throwable t) {
-                            mainError.addSuppressed(t);
-                        }
-                    } else {
-                        JPASingleton.getInstance().closeEntityManager();
-                    }
-            }
-        }catch (BusinessException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new InfraException("Falha crítica durante a execução da transação.", e);
-        }
+       execute(em -> {
+          action.accept(em);
+          return null;
+       });
     }
 
     public static <T> T execute(Function<EntityManager, T> action) {
